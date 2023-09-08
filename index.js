20f09c633d530a6ed6a5f160c7e8bdb7ce881c0d
Chaman Rajoriya <103516146+ChamanRajoriya@users.noreply.github.com>
2023-09-08 19:58:29 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function show(){
+        
+    document.getElementById("navList").style.right = "0";
+    document.getElementById("navList").style.display = "block";
+}
+function hide()
+{
+   
+    document.getElementById("navList").style.right ="-200px";
+    document.getElementById("navList").style.display = "none";
+}
